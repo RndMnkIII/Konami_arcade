f340861ebe9a70f42c09957cafd9d7d8e56f604a
RndMnkIII <rndmnkiii@gmail.com>
2020-01-23 20:11:42 +0100

    Update PAL16L8_053327_D20.v

--- Aliens/verilog/PAL16L8_053327_D20.v
+++ Aliens/verilog/PAL16L8_053327_D20.v
@@ -1,42 +1,36 @@
-//PAL16L8_053326_D21.v
+//PAL16L8_053327_D20.v
 //Implementation of PAL 053327-D20 combinatorial logic
 //used in Aliens Konami Arcade PCB.
 //Converted to Verilog By RndMnkIII. 10/2019. (@RndMnkIII).
 //---------------------------------------------------------
 //Dumped from an unsecured PAL16L8, 
 //converted to GAL16V8 with PALTOGAL 
-//and tested by Caius (@Caius63417737).
+//and tested by Caius (@caiusarcade).
+// https://jammarcade.net
 //---------------------------------------------------------
-`timescale 1ns / 1ns
-module PAL16L8_053327_D20(in, out);
-	input [11:0] in;
-	output [4:0] out;
-	//wire o14, o15;
-	reg r14, r15;
+`default_nettype none
+`timescale 1 ns / 100 ps
+module PAL16L8_053327_D20( 
+	input D18_5, D21_17, RMRD, MAA, MA9, MA8, MA7, D21_12, C20_6, C20_3, D21_15, D21_16,
+	output D20_12, IOCS, CRAMCS, VRAMCS, OBJCS);
 	
-	always @(in[4] or in[5] or in[6] or in[8] or in[10])
-	begin
-	    #5  r14 = ~(in[4] & in[5] & in[6] & ~in[8] & ~in[10]);
-	end
+	wire R14, R15;
 	
-	always @(in[0] or in[2] or in[3] or in[4] or in[5] or in[6] or in[8] or in[9] or in[11])
-	begin
-	   #5 r15 = ~((~in[0] & ~in[2] & ~in[3] & ~in[4] & ~in[5] & ~in[6] & ~in[8] & ~in[9] & ~in[11]) | (~in[0] & ~in[2] & in[3] & ~in[11]));
-	end 
-	
-	//assign o14 = ~(in[4] & in[5] & in[6] & ~in[8] & ~in[10]);
-	
-	//assign o15 = ~(~in[0] & ~in[2] & ~in[3] & ~in[4] & ~in[5] & ~in[6] & ~in[8] & ~in[9] & ~in[11] + ~in[0] & ~in[2] & in[3] & ~in[11]);
-	
-	//assign out[0] = ~(~in[0] & ~in[2] & ~in[3] & ~in[4] & ~in[5] & ~in[6] & ~in[8] & ~in[9] + ~in[0] & ~in[2] & in[3]  + ~in[0] & ~in[1] & ~o14 & ~o15);
-	assign #5 out[0] = ~((~in[0] & ~in[2] & ~in[3] & ~in[4] & ~in[5] & ~in[6] & ~in[8] & ~in[9]) | (~in[0] & ~in[2] & in[3])  | (~in[0] & ~in[1] & ~r14 & ~r15));
-	//assign out[1] = o14;
-	assign out[1] = r14;
+	assign #10 D20_12 = ~((~D18_5 & ~RMRD & ~MAA & ~MA9 & ~MA8 & ~MA7 & ~C20_6 & ~C20_3 & ~D21_16) |
+       (~D18_5 & ~RMRD & MAA & ~D21_16) |
+       (~D18_5 & ~D21_17 & R14 & R15));
 	   
-	assign #5 out[2] = ~(~in[0] & ~in[7]);
+	assign #7 R14 = ~(MA9 & MA8 & MA7 & ~C20_6 & ~D21_15);
+	
+	assign #7 IOCS = R14;
+	
+	assign #7 R15 = ~((~D18_5 & ~RMRD & ~MAA & ~MA9 & ~MA8 & ~MA7 & ~C20_6 & ~C20_3 & ~D21_16) |
+       (~D18_5 & ~RMRD & MAA & ~D21_16));
+	   
+	assign #7 CRAMCS = ~(~D18_5 & ~D21_12);
 
-	//assign out[3]  = ~(~in[0] & ~in[1] & in[7] & ~o14 & ~o15);
-	assign #5 out[3]  = ~(~in[0] & ~in[1] & in[7] & ~r14 & ~r15);
-
-	assign #5 out[4]  = ~((~in[0] & ~in[2] & ~in[3] & ~in[4] & ~in[5] & ~in[6] & ~in[8] & ~in[9] & ~in[11]) | (~in[0] & ~in[2] & in[3]));
-endmodule	   
+	assign #10 VRAMCS = ~(~D18_5 & ~D21_17 & D21_12 & R14 & R15);
+	
+	assign #7 OBJCS = ~((~D18_5 & ~RMRD & ~MAA & ~MA9 & ~MA8 & ~MA7 & ~C20_6 & ~C20_3 & ~D21_16) |
+       (~D18_5 & ~RMRD & MAA & ~D21_16));
+endmodule  	   
